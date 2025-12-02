07d1ffc32681f1b724d6ff792125fd591f1067d7
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-12-03 00:38:58 +0600

    Add register, login controller

---
+++ server/src/controller/authController.js
@@ -0,0 +1,19 @@
+import * as authService from '../services/authServices.js';
+
+export const registerUser = async (req, res) => {
+  try {
+    const userData = await authService.registerUserService(req.body);
+    res.status(201).json(userData);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const userData = await authService.loginUserService(req.body);
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
