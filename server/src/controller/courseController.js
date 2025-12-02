4bd3de97ae1383706aa0b2f0be40d8acc0dbb657
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-12-03 01:56:28 +0600

    Add create, get, get by id courses controller

---
+++ server/src/controller/courseController.js
@@ -0,0 +1,29 @@
+import * as courseServices from '../services/courseServices.js';
+
+
+export const createCourse = async (req, res) => {
+  try {
+    const course = await courseServices.createCourseService(req.body);
+    res.status(201).json(course);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const getAllCourses = async (req, res) => {
+  try {
+    const result = await courseServices.getAllCoursesService(req.query);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getCourseById = async (req, res) => {
+  try {
+    const course = await courseServices.getCourseByIdService(req.params.id);
+    res.status(200).json(course);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
