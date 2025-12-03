5c75006c1fcbb3c84ede6e2616d3b0c468af7a67
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-12-03 14:32:34 +0600

    Add course enrollment model

---
+++ server/src/models/Enrollment.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const enrollmentSchema = new mongoose.Schema({
+  student: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  },
+  course: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Course', 
+    required: true 
+  },
+  
+  completedLessons: [{ 
+    type: mongoose.Schema.Types.ObjectId 
+  }],
+  
+  progress: { 
+    type: Number, 
+    default: 0,
+    max: 100
+  },
+  isCompleted: { 
+    type: Boolean, 
+    default: false 
+  }
+}, { timestamps: true });
+
+enrollmentSchema.index({ student: 1, course: 1 }, { unique: true });
+
+export default mongoose.model('Enrollment', enrollmentSchema);
